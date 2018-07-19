6dbba7ddb769ff60b678395c4818b29115e3b6fb
Maxon <marokhonko@gmail.com>
2018-07-19 13:07:16 +0300

    Fix for Ruslan

--- build/dest/js/js.js
+++ build/dest/js/js.js
@@ -88,6 +88,30 @@ $(function() {
 
 	$('.popup').magnificPopup();
 
+	var inputs = document.querySelectorAll('.inputfile');
+	Array.prototype.forEach.call(inputs, function(input) {
+		var label = input.nextElementSibling,
+			labelVal = label.innerHTML;
+
+		input.addEventListener('change', function(e) {
+			var fileName = '';
+			if (this.files && this.files.length > 1)
+				fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
+			else
+				fileName = e.target.value.split('\\').pop();
+
+			if (fileName)
+				label.querySelector('span').innerHTML = fileName;
+			else
+				label.innerHTML = labelVal;
+		});
+	});
+
+	// $('select').on('change', function () {
+	//     if($(this).val() == '1'){
+	//         alert();
+	//     }
+	// })
 
 
 });
