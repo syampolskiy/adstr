7ac6d17e5c0262601ee5ac311ff4232123e6e2cc
Maxon <marokhonko@gmail.com>
2018-08-03 14:24:58 +0300

    full chart

--- build/dest/js/js.js
+++ build/dest/js/js.js
@@ -131,25 +131,6 @@ $(function() {
 
 });
 $(function() {
-	// menu dropdowns
-	$('.js-toggle-submenu').click(function(e) {
-		e.preventDefault();
-
-		var self = $(this);
-		self.siblings('.js-toggle-me').slideToggle(100, function() {
-			self.parent().toggleClass('opened');
-		});
-	})
-	// menu dropdowns --/
-
-	// toggle sidebar
-	$('.js-toggle-sidebar').click(function() {
-		$('.js-toggle-it-on-mobile').slideToggle(100);
-		$(this).toggleClass('collapsed');
-	});
-	// toggle sidebar --/
-});
-$(function() {
 	// dropdowns
 	$('.js-toggle-dropdown').click(function() {
 		$(this).toggleClass('opened');
@@ -173,4 +154,23 @@ $(function() {
 		});
 	}
 	// dropdowns --/
+});
+$(function() {
+	// menu dropdowns
+	$('.js-toggle-submenu').click(function(e) {
+		e.preventDefault();
+
+		var self = $(this);
+		self.siblings('.js-toggle-me').slideToggle(100, function() {
+			self.parent().toggleClass('opened');
+		});
+	})
+	// menu dropdowns --/
+
+	// toggle sidebar
+	$('.js-toggle-sidebar').click(function() {
+		$('.js-toggle-it-on-mobile').slideToggle(100);
+		$(this).toggleClass('collapsed');
+	});
+	// toggle sidebar --/
 });
