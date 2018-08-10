069c0051dc187949ffbf294677c51747256c0965
maxon <marokhonko@gmail.com>
2018-08-10 16:47:30 +0300

    fix 2

--- build/dest/js/js.js
+++ build/dest/js/js.js
@@ -140,6 +140,25 @@ $(function() {
 
 });
 $(function() {
+	// menu dropdowns
+	$('.js-toggle-submenu').click(function(e) {
+		e.preventDefault();
+
+		var self = $(this);
+		self.siblings('.js-toggle-me').slideToggle(100, function() {
+			self.parent().toggleClass('opened');
+		});
+	})
+	// menu dropdowns --/
+
+	// toggle sidebar
+	$('.js-toggle-sidebar').click(function() {
+		$('.js-toggle-it-on-mobile').slideToggle(100);
+		$(this).toggleClass('collapsed');
+	});
+	// toggle sidebar --/
+});
+$(function() {
 	// dropdowns
 	$('.js-toggle-dropdown').click(function() {
 		$(this).toggleClass('opened');
@@ -163,23 +182,4 @@ $(function() {
 		});
 	}
 	// dropdowns --/
-});
-$(function() {
-	// menu dropdowns
-	$('.js-toggle-submenu').click(function(e) {
-		e.preventDefault();
-
-		var self = $(this);
-		self.siblings('.js-toggle-me').slideToggle(100, function() {
-			self.parent().toggleClass('opened');
-		});
-	})
-	// menu dropdowns --/
-
-	// toggle sidebar
-	$('.js-toggle-sidebar').click(function() {
-		$('.js-toggle-it-on-mobile').slideToggle(100);
-		$(this).toggleClass('collapsed');
-	});
-	// toggle sidebar --/
 });
